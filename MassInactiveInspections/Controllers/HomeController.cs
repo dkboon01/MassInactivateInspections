229f207fe5b9e151f63905002f5e57cb5eeaa561
Brandon Roberts <broberts@silcofireprotection.com>
2017-07-12 12:17:45 -0400

    Add project files.

---
+++ MassInactiveInspections/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+using MassInactiveInspections.Models;
+//using System.Net.Http.WebRequests;
+
+namespace MassInactiveInspections.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
+
+
---
+++ MassInactiveInspections/Models/Helper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+
+using System.Net;
+using System.IO;
+using System.Configuration;
+using System.Data;
+using System.Collections;
+using MassInactiveInspections;
+
+using System.Data.Entity;
+
+using System.Data.SqlClient;
+using MassInactiveInspections.Models;
+using System.Globalization;
+
+namespace MassInactiveInspections.Models
+{
+
+    public class Helper
+    {
+        //public static List<GetAuthorizationForLeads_Result> GetAuthorizationforLeads<T>(string username, int appid)
+
+        //where T : class
+        //{
+           
+        //    var db = new Cust_SilcoEntities();
+        //    List<GetAuthorizationForLeads_Result> authorizations = db.GetAuthorizationForLeads(username, appid).ToList();
+        //    return authorizations;
+        //}
+        public static List<GetADGroups_Result> GetAD_Groups<T>(int appid) where T : class
+        {
+            var db = new Cust_SilcoEntities();
+            List<GetADGroups_Result> groups = db.GetADGroups(appid).ToList();
+            //foreach (var grp in groups)
+            //{
+            //    grp.adsecuritygroupallowed
+            //}
+            // List<GetAuthorizationForLeads_Result> groups = db.GetAuthorizationForLeads(appid).ToList();  
+            return groups;
+        }
+        public static List<FindSecurityMassInact_Result> FindSecurity<T>(int appid, string title, string dept, string account) where T : class
+        {
+
+            var sb = new Cust_SilcoEntities();
+
+            List<FindSecurityMassInact_Result> seclist = sb.FindSecurityMassInact(appid, title, dept, account).ToList();
+            return seclist;
+        }
+
+    }
+}
+
+
